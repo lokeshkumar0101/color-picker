9f157113f6625e54efa37e1cd4a810db80071c18
Lokesh Kumar <lokeshkumar.080806@gmail.com>
2024-09-01 19:51:56 +0530

    fixing some issues of footer and header and adding shadow on hover on main components

--- src/App.jsx
+++ src/App.jsx
@@ -19,16 +19,17 @@ function App() {
   }
 
   return (
-    <div className="h-screen">
-      <h2 className="text-black font-bold text-3xl py-5 rounded-b-full border-2 text-center bg-white" style={{ borderColor: color }}>
+    <div className="h-full">
+      <h2 className="text-black font-bold text-3xl py-5 rounded-b-3xl border-2 text-center bg-[#ffffff] " style={{ borderColor: color }}>
         Color Picker
       </h2>
       <div className="flex justify-center ">
         <div
-          className="h-48 w-48 border-zinc-200 border-[2px] rounded-2xl text-center text-zinc-200 font-bold mt-16 flex items-center justify-center"
+          className="h-48 w-48 border-zinc-200 border-[2px] rounded-2xl text-center text-zinc-200 font-bold mt-16 flex items-center justify-center hover:shadow-md hover:shadow-white ease-in duration-200"
           style={{ backgroundColor: color }}
         >Color Code: <br /> {color}</div>
       </div>
+      
       <div className="flex justify-center">
         <label className="text-xl mt-10 font-bold text-white">👇Select a Color👇</label>
       </div>
@@ -37,12 +38,12 @@ function App() {
           type="color"
           value={color}
           onChange={handleColor}
-          className="h-10 w-28 border-[1.5px] border-white mt-5 rounded-md shadow-md shadow-black "
+          className="h-10 w-28 border border-white mt-5 rounded-md hover:shadow-md hover:shadow-white ease-in duration-200 "
         />
       </div>
-      <div className="flex justify-center mt-10 ">
+      <div className="flex justify-center mt-10">
         <h3
-          className="text-center  border-zinc-200 border-[1px] w-48 rounded-md py-3 text-white font-bold "
+          className="text-center  border-zinc-200 border-[1px] w-48 rounded-md py-3 text-white font-bold hover:shadow-md hover:shadow-white ease-in duration-200"
           style={{ backgroundColor: color }}
         >
           Picked Color: {color}
@@ -50,9 +51,8 @@ function App() {
         <button onClick={()=>{
           copyColor();
           handleText();
-        }} className=" ml-3 px-7 font-bold text-xl text-white rounded-md border-[1px] border-white bg-blue-600 hover:bg-blue-700">{text}</button>
+        }} className=" ml-3 px-7 font-bold text-xl text-white rounded-md border-[1px] border-white bg-blue-700 hover:bg-blue-600 active:text-stone-300">{text}</button>
       </div>
-
       <Footer/>
     </div>
   );
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='w-full'>
-        <p className='py-4 text-center mt-10 text-white border-t-2 border-dotted border-white text-sm'>Made by @Lokesh Kumar</p>
-    </div>
+    <>
+      <p className='py-4 text-center mt-10 text-white border-t-2 border-dotted border-white text-sm'>Made by @Lokesh Kumar</p>
+    </>
   )
 }
 
